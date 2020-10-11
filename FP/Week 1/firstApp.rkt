fba5c098afe9a143b2e496385017100939a87f7c
5ko99 <petko.krasimirov99@gmail.com>
2020-10-11 14:30:37 +0300

    Some basic and begginers functions on Scheme

--- FP/Week 1/firstApp.rkt
+++ FP/Week 1/firstApp.rkt
@@ -23,3 +23,50 @@
   )
 
 (fact 5)
+
+(define (fib n)
+  (define (helper n-1 n-2 curr limit)
+      (if (> curr limit)
+      n-1
+      (helper (+ n-1 n-2) n-1 (+ curr 1) limit)
+      
+    ))
+   (helper 1 1 3 n)
+  )
+(fib 8)
+
+(define (sum-int x y)
+  (if(> x y) 0
+   (+ x (sum-int (+ x 1) y)))
+)
+
+(define (pow x n)
+  (if (= n 0) 1
+  (* x (pow x (- n 1)))
+  ))
+
+(define (a n)
+  (if (< n 0) (- n)
+  n
+  ))
+(a -8)
+
+(define (isPrime? n)
+  (define (helper current-number)
+    (cond
+      ((>= current-number (/ n 2)) #t)
+      ((= (remainder n current-number) 0) #f)
+      (else (helper (+ current-number 1)))
+      )
+    )
+  (if (< n 2)
+      #f
+      (helper 2)
+      )
+)
+
+(isPrime? 6)
+(isPrime? 2)
+(isPrime? 7)
+(isPrime? 66)
+(isPrime? 101)
