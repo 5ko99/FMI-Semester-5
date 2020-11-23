d87b8b18d7ca6b1120df6754ccba820c74bd33cf
5ko99 <petko.krasimirov99@gmail.com>
2020-11-23 20:53:40 +0200

    string to list function for lists

--- FP/Homework/test-tree.rkt
+++ FP/Homework/test-tree.rkt
@@ -6,9 +6,18 @@
  (test-suite
   "tree? testes"
   (test-case "Valid Trees"
-             (check-true(tree? "{5 * {2 * *}}"))
+             (check-true (tree? "{5 * {2 * *}}"))
+             (check-true(tree? "{2**}"))
+             (check-true(tree? "{2     {4     * *}               *}"))
+             (check-true(tree? "*"))
              )
+  
   (test-case "UnvalidTrees"
-             (check-false (tree? "{5 * {2 * *}"))
-             )
-  ))
+              (check-false (tree? "{5 * {2 * *}"))
+              (check-false (tree? "{5 * {*}}"))
+              (check-false (tree? "abcd"))
+              (check-false (tree? "{2 *{3 * *} {3 * {2 *}"))
+              (check-false (tree? "{2 {2 * * *} *}"))
+              )
+  )
+ )
--- FP/Homework/tree.rkt
+++ FP/Homework/tree.rkt
@@ -35,7 +35,7 @@
           [(and (is-digit? cur) (not node?)) (loop (+ i 1) node? left? right? final?)]
           [(and (is-digit? cur) node?) #f];veche e procheteno node, a pak se poqvqva chislo
           [(eq? cur #\*)
-           (cond [(not node?) #f];leef before node
+           (cond [(and (not node?) (not (is-digit? (string-ref str (- i 1))))) #f];leef before node
                  [(not left?) (loop (+ i 1) #t #t #f final?)]
                  [(and left? (not right?)) (loop (+ i 1) #t #t #t final?)]
                  [else #f])]
@@ -50,14 +50,59 @@
           ;[(eq? cur #\}) (and node? left? right?)]
           [else #f]
   ))))
-  (trace loop)
-  (if (eq? (string-ref str 0) #\{) (loop 1 #f #f #f final?) #f)
+;  (trace loop)
+  (if (zero? (string-length str)) #f
+  (if (eq? (string-ref str 0) #\{) (loop 1 #f #f #f final?) (if (and (= (string-length str) 1) (eq? (string-ref str 0) #\*)) #t #f))
+  ))
+;(trace tree?-helper)
+
+;helper function for work with trees
+(define empty-tree '())
+(define (make-tree root left right) (list root left right))      ; не искаме просто (define make-tree list) - защо?
+(define (make-leaf root) (make-tree root empty-tree empty-tree)) ; за удобство
+(define root-tree car)
+(define left-tree cadr)
+(define right-tree caddr)
+(define empty-tree? null?)
+(define (length lst)
+  (cond
+    [(empty? lst)  0]
+    [(cons? lst)   (+ 1 (length (tail lst)))]))
+
+(define (string->tree-helper str)
+  (define (loop i node left right lset rset)
+    (let ((cur (string-ref str i)))
+    (cond [(eq? cur #\}) (cons (make-tree (string->number node) left right) i)]
+          [(is-digit? cur) (loop (+ i 1) (string-append node (string cur)) left right lset rset)]
+          [(eq? cur #\*) (if (not lset) (loop (+ i 1) node empty-tree right #t #f) (loop (+ i 1) node left empty-tree #t #t))]
+          [(is-space? cur) (loop (+ i 1) node left right lset rset)]
+          [(eq? cur #\{)
+           (let* ((treePair (string->tree-helper (substring str i)))
+                 (tree (head treePair))
+                 (len (tail treePair)))
+             (if (not lset)
+                 (loop (+ i len 1) node tree right #t #f)
+                 (loop (+ i len 1) node left tree #t #t)))]
+          )))
+  (loop 1 "" `() `() #f #f)
   )
-(trace tree?-helper)
+
+
 
 (define (tree? input)
   (let((result (tree?-helper input #t)))
     (if (not result) #f #t)))
+
+(define (string->tree input)
+  (define str (string-normalize-spaces input))
+  (if (and (= (string-length str) 1) (eq? (string-ref str 0) #\*)) empty-tree 
+  (if (tree? str) (head (string->tree-helper str)) #f)
+  ))
+
+(string->tree "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}}}")
+(string->tree "{1 * {3 {111 * *} *}}")
+(string->tree "{11515 {5 {6 {7 * *} *} {100 * *}} *}")
+(string->tree "*")
 
 (provide (all-defined-out))
   
