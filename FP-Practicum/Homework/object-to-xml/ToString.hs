fc49ffe54deb6b0172b977c58657b35ae88aa900
5ko99 <petko.krasimirov99@gmail.com>
2021-01-16 19:11:18 +0200

    Added parser from xmlObject to string

---
+++ FP-Practicum/Homework/object-to-xml/ToString.hs
@@ -0,0 +1,26 @@
+import XMLObject
+
+attributeToString :: Attribute -> [Char]
+attributeToString (a, b) = a ++ "=" ++ "\"" ++ b ++ "\""
+
+attributesToStrings :: [Attribute] -> [String]
+attributesToStrings = map attributeToString
+
+stringsToString :: [String] -> String
+stringsToString [] = ""
+stringsToString (x : xs) = x ++ " " ++ stringsToString xs
+
+attributesToString :: [Attribute] -> String
+attributesToString = stringsToString . attributesToStrings
+
+tagElementToString :: TagElement -> String
+tagElementToString (TagElement str atr xmlObj) =
+  "<" ++ str ++ " " ++ attributesToString atr ++ ">"
+    ++ stringsToString (map xmlToString xmlObj)
+    ++ "</"
+    ++ str
+    ++ ">"
+
+xmlToString :: XMLObject -> String
+xmlToString (Text text) = text
+xmlToString (Element el) = tagElementToString el
---
+++ FP-Practicum/Homework/object-to-xml/XMLObject.hs
@@ -0,0 +1,14 @@
+module XMLObject where
+
+type Attribute = (String, String)
+
+data TagElement = TagElement
+  { name :: String,
+    attributes :: [Attribute],
+    children :: [XMLObject]
+  }
+  deriving (Show, Read, Eq)
+data XMLObject
+  = Text String
+  | Element TagElement
+  deriving (Show, Read, Eq)
