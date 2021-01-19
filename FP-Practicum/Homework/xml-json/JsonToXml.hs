d44127c5a46bcd659437766d2af811d4fd40c517
5ko99 <petko.krasimirov99@gmail.com>
2021-01-19 20:49:01 +0200

    Nearly finished conversation

--- FP-Practicum/Homework/xml-json/JsonToXml.hs
+++ FP-Practicum/Homework/xml-json/JsonToXml.hs
@@ -1,20 +1,45 @@
 module JsonToXml where
 
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Maybe
 import JsonObjectMod
 import XMLObjectMod
 
+jsonListToXml :: [(String, JsonValue)] -> [XMLObject]
+jsonListToXml [] = []
+jsonListToXml lst@((name, json@(JsonObject pair)) : xs) =
+  Element (TagElement name attributes (jsonToXmlObjL json)) : jsonListToXml xs
+  where
+    attributes = getAttributes pair
+jsonListToXml ((name, json) : xs)
+  | name == "Text" = Text (getString json) : jsonListToXml xs
+  | head name /= '-' = Element (TagElement name [] (jsonToXmlObjL json)) : jsonListToXml xs
+  | otherwise = jsonListToXml xs
+
+getString :: JsonValue -> String
+getString (JsonString str) = str
+
+jsonArrToXml :: [JsonValue] -> [XMLObject]
+jsonArrToXml arr = concat mapped
+  where
+    mapped = map jsonToXmlObjL arr
+
+getAttributes :: [(String, JsonValue)] -> [Attribute]
+getAttributes [] = []
+getAttributes ((name, JsonString str) : xs)
+  | head name == '-' = (drop 1 name, str) : getAttributes xs
+  | otherwise = getAttributes xs
+getAttributes (_ : xs) = getAttributes xs
+
+jsonToXmlObjL :: JsonValue -> [XMLObject]
+jsonToXmlObjL JsonNull = [Text "null"]
+jsonToXmlObjL (JsonString str) = [Text str]
+jsonToXmlObjL (JsonBool b) = [Text $ show b]
+jsonToXmlObjL (JsonNumber n) = [Text $ show n]
+jsonToXmlObjL (JsonArray arr) = jsonArrToXml arr
+jsonToXmlObjL (JsonObject arr) = jsonListToXml arr
+
 jsonToXml :: JsonValue -> XMLObject
-jsonToXml JsonNull = Text "null"
-jsonToXml (JsonString str) = Text str
-jsonToXml (JsonBool b) = Text $ show b
-jsonToXml (JsonNumber n) = Text $ show n
-jsonToXml (JsonArray arr) = Element $ TagElement "p" [] (toArray "p" arr) --TODO change this hardcoded p
-    where
-        toArray :: String -> [JsonValue] -> [XMLObject]
-        toArray _ [] = []
-        toArray s (x:xs) =  Element (TagElement s [] [Text $ show x]) : toArray s xs
-jsonToXml (JsonObject arr) = Element (TagElement "main" [] (helper arr))
-    where
-        helper :: [(String, JsonValue)] -> [XMLObject]
-        helper [] = []
-        helper ((s, val):xs) = Element (TagElement s [] [jsonToXml val]) : helper xs
+jsonToXml json = Element (TagElement "xml" [] lst)
+  where
+    lst = jsonToXmlObjL json
